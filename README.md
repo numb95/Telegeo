e9f317810c20dc9ba8df0175c964c95f57782def
AmirHossein Goodarzi <amir@goodarzi.net>
2018-11-08 01:17:36 +0330

    refactoring and change method of getting token from opening a text file to a enviroment variable

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ based on your location, it says what is near you
 
 ### *Still under development*
 
-use pipenv to install dependencies. Google it. Create a file and name it ```token.txt``` place your token in it. 
+use pipenv to install dependencies. Google it. open your shell config and add you token to a BOT_TOKEN enviroment variable. For example you can add ```export BOT_TOKEN='TOKEN'``` to your shell config. 
 
 it's just recieve your location and send you the google map address. Will add what is going on near you. 
 
--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 from telegram.ext import Updater,CommandHandler, MessageHandler, Filters 
 import logging
-
-with open('token.txt','r') as t:
-    token = t.read().replace('\n','')
-
-updater = Updater(token=token)
-
-logging.basicConfig(format='%(asctime)s - %(name)s %(levelname)s - %(message)s', level=logging.INFO)
+import os
 
 welcome_message = "سلام به بات اینجا چه خبره خوش اومدی. قبل از هر کاری لوکیشنت رو برام بفرست تا ببینم اطرافت چی داری"
+
 def start(bot, update):
+    '''
+    
+    '''
     bot.send_message(chat_id=update.message.chat_id, text=welcome_message)
 
-map_url = 'https://www.google.com/maps/@{0},{1},15z'
 def get_loc(bot, update):
+
+    map_url = 'https://www.google.com/maps/@{0},{1},15z'
     bot.send_message(chat_id=update.message.chat_id, text=map_url.format(update.message.location.latitude, update.message.location.longitude))
 
-start_handler = CommandHandler('start', start)
-updater.dispatcher.add_handler(start_handler)
 
-get_loc_handler = MessageHandler(Filters.location, get_loc)
-updater.dispatcher.add_handler(get_loc_handler)
+def main():
 
-updater.start_polling()
-updater.idle()
+    token = os.getenv('BOT_TOKEN')
+    updater = Updater(token=token)
+
+    logging.basicConfig(format='%(asctime)s - %(name)s %(levelname)s - %(message)s', level=logging.INFO)
+
+    start_handler = CommandHandler('start', start)
+    updater.dispatcher.add_handler(start_handler)
+
+    get_loc_handler = MessageHandler(Filters.location, get_loc)
+    updater.dispatcher.add_handler(get_loc_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
