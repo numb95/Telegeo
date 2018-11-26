46305d2fc4e26a3d9efca0c211e538957f080fd9
AmirHossein Goodarzi <amir007ag@gmail.com>
2018-11-26 17:21:49 +0330

    nothing :))

--- main.py
+++ main.py
@@ -4,46 +4,50 @@ import logging
 import os
 import sqlite3
 from pathlib import Path
-welcome_message = "سلام. برای اینکه بدونیم چه اتفاقی افتاده لازمه که به ما بگی دقیقاً کی این اتفاق افتاد اگر همین الان بود روی دکمه همین الان و اگه یه روز دیگه بود دکمه مرتبطش رو انتخا کن"
+from xlsxwriter.workbook import Workbook
+
+time_date = ""
+welcome_message = "سلام. برای اینکه بفهمیم این اتفاق در چه روزی افتاده لازمه که بهمون تاریخ رو بگی. حتی اگه امروز این اتفاق افتاده باشه تاریخ امروز رو وارد کن. تاریخ رو هم به فرمت مثال زیر برامون بفرست: 1397/08/29"
 location_message = "حالا برای اینکه بدونیم چه اتفاقی افتاده لوکیشنت رو برامون به کمک دکمه زیر بفرست"
 type_message=" در ضمن نوع مشکلت رو هم مشخص کن"
 get_loc_message = "با سپاس از شما، اطلاعات شما دریافت شد"
 
 def start(bot, update):
     #print(update)
-    keyboard = [[InlineKeyboardButton("همین الان", callback_data='همین الان', request_location=True),
-                InlineKeyboardButton("یه روز دیگه", callback_data='یه روز دیگه')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text(welcome_message, reply_markup=reply_markup)
+    bot.send_message(text=welcome_message, chat_id=update.message.chat_id)
+
+
+def times(bot, update):
     type_keyboard = [[InlineKeyboardButton("تصادف", callback_data='تصادف'),
-                     InlineKeyboardButton("مشکل جاده", callback_data='مشکل جاده')]]
+                     InlineKeyboardButton("مشکل جاده", callback_data='مشکل جاده'),
+                      InlineKeyboardButton("ترافیک", callback_data='ترفیک'),
+                      InlineKeyboardButton("پلیس", callback_data='پلیس')
+                      ]]
     type_reply_markup = InlineKeyboardMarkup(type_keyboard)
     update.message.reply_text(type_message, reply_markup=type_reply_markup)
 
 
 def button(bot, update):
-    #print(update)
     query = update.callback_query
     bot.edit_message_text(text="%s گزینهٔ انتخابی شما" % query.data,
         chat_id=query.message.chat_id,
         message_id=query.message.message_id)
+    
     query = update.callback_query
     bot.send_message(text="حالا موقعیت مکانی‌ت رو هم برام بفرست",
         chat_id=query.message.chat_id)
 
 
 
-
-
 def get_loc(bot, update):
-
-
+    print (button(bot, update))
     sender_data = update.message.from_user
     user_id = sender_data['id']
     latitude = update.message.location.latitude
     longitude = update.message.location.longitude
     admin_id = 0
     date = update.message.date
+    serv = 0
     try:
         username = sender_data['username']
     except:
@@ -52,24 +56,41 @@ def get_loc(bot, update):
         user_name = 'undefiend'
     else:
         user_name = sender_data['first_name'] + ' ' + sender_data['last_name']
-    store_db(user_id, user_name, username, latitude, longitude, admin_id, date, type)
+    store_db(user_id, user_name, username, latitude, longitude, admin_id, date, serv)
     bot.send_message(chat_id=update.message.chat_id, text=get_loc_message)
 
 def make_table_db():
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
     cursor.execute('''CREATE TABLE datas
-                    (id INTEGER, name TEXT DEFAULT 'undefined', username TEXT DEFAULT 'undefiend', lat REAL, long REAL, is_admin INTEGER DEFAULT 0, date TEXT, type TEXT)
+                    (id INTEGER, name TEXT DEFAULT 'undefined', username TEXT DEFAULT 'undefiend', lat REAL, long REAL, is_admin INTEGER DEFAULT 0, date TEXT, serv TEXT DEFAULT 0)
                     ''')
     connection.commit()
     connection.close()
 
-def store_db(user_id, user_name, username, latitude, longitude, admin_id, date, type):
+def store_db(user_id, user_name, username, latitude, longitude, admin_id, date, serv):
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO datas VALUES (?,?,?,?,?,?,?,?)",(user_id, user_name, username, latitude, longitude, admin_id, date, type))
+    cursor.execute("INSERT INTO datas VALUES (?,?,?,?,?,?,?,?)",(user_id, user_name, username, latitude, longitude, admin_id, date, serv))
     connection.commit()
     connection.close()
+
+
+
+def get_db(bot, update):
+    workbook = Workbook('database.xlsx')
+    worksheet = workbook.add_worksheet()
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("select * from datas")
+    mysel = c.execute("select * from datas")
+    for i, row in enumerate(mysel):
+        for j, value in enumerate(row):
+            worksheet.write(i, j, value)
+    workbook.close()
+    bot.send_document(chat_id=update.message.chat_id, document=open('database.xlsx', 'rb'))
+    bot.send_document(chat_id=update.message.chat_id, document=open('database.db', 'rb'))
+
 
 def main():
     if Path("./database.db").is_file():
@@ -81,10 +102,18 @@ def main():
     updater = Updater(token=token)
 
     logging.basicConfig(format='%(asctime)s - %(name)s %(levelname)s - %(message)s', level=logging.INFO)
-
+    print (time_date)
     start_handler = CommandHandler('start', start)
+    times_handler = MessageHandler(Filters.text, times)
+    getdb_handler = CommandHandler('get_db', get_db)
+
     updater.dispatcher.add_handler(start_handler)
+    updater.dispatcher.add_handler(times_handler)
+    updater.dispatcher.add_handler(getdb_handler)
+
+    print(time_date)
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
     get_loc_handler = MessageHandler(Filters.location, get_loc)
     updater.dispatcher.add_handler(get_loc_handler)
     updater.start_polling()
